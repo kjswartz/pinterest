1def6adecf7d9a42b3356110e9d176047b8bcab3
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-03-19 17:53:59 -0400

    Write a functional test to ensure the home page comes up with the proper data on it.

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,8 +1,9 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
-  test "should get index" do
+  test "all pins should " do
     get :index
+    assert_template layout: "layouts/application", partial: "_navbar"
     assert_response :success
   end
 
