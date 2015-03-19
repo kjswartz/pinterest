1c0276c0674b13e2ba257480aa3331b1bc2d0bac
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-03-19 17:56:38 -0400

    modified test should phrase.

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
-  test "all pins should " do
+  test "should get welcome index and application navbar partial" do
     get :index
     assert_template layout: "layouts/application", partial: "_navbar"
     assert_response :success
