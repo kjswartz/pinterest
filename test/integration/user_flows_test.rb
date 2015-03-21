a7cff91f25df8c7ee53a244de89028eed7e6788c
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-03-21 15:35:57 -0400

    made it so a new user is created and then tested browsing as that user is logged in.

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -1,12 +1,32 @@
 require 'test_helper'
+require 'bcrypt'
 
 class UserFlowsTest < ActionDispatch::IntegrationTest
-  test "user login and browses" do
-    https!
-    get root_path
+
+  test "user signup, login and browse" do
+    get '/users/new'
     assert_response :success
 
-    post_via_redirect "/session/signin", email: users(:one).email, password: users(:one).password
-    assert_equal session_signin_path, path
+    post "/users", user: { about: 'about', avatar: 'avatar', email: 'tom@tom.com', location: 'tampa', name: 'tom', username: '12345', password: 'password' }
+    assert_redirected_to user_path(session[:user_id])
+    @user = User.find_by(id: session[:user_id])
+
+    assert_equal @user.pins.count, 0
+
+    get "/users/#{User.first.id}"
+    assert_response :redirect
+
+    get "/users/#{@user.id}"
+    assert_response :success
+
+
+    # 980190963
+
+    # assert_equal session_signin_path, path
+    # assert_equal 200, status
+    # assert_equal "/home", path
+
+    # get '/admin'
+    # assert_equal 200, status
   end
 end
