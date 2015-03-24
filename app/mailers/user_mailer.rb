a86d4adb8802e7f2acc554b584914f6505ee5248
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-03-23 20:50:53 -0400

    changed email subject line.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @url = 'https://secure-mountain-7773.herokuapp.com/'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    mail(to: @user.email, subject: 'Welcome to Pinterest')
   end
 end
