318e79d26b0831aa006ac879d29c433d7f717d46
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-03-23 20:12:54 -0400

    added action mailer.

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ApplicationMailer
+
+  def welcome_email(user)
+    @user = user
+    @url = 'https://secure-mountain-7773.herokuapp.com/'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+end
